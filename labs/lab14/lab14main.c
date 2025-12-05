56a2f78bb67f38a377113042b5b231d70ab5c1d6
Grayson Mueller <mueller_gm@yahoo.com>
2025-12-05 16:19:55 -0600

    update the main file for lab 14

--- labs/lab14/lab14main.c
+++ labs/lab14/lab14main.c
@@ -1,13 +1,24 @@
 #include "lab14.h"
+#include "string.h"
 
 int main() {
     struct Reading *arr = createReadings(10);
     struct Reading *p = arr;
     struct Reading *end = arr + 10;
 
+    const char* status_array[] = {"OK", "WARNING", "ERROR"};
+    
+    initReading(arr + 4, 104, 28.4, STATUS_ERROR);
+
     while(p < end) {
-        printf("%d ", p->id);
-        printf("%d\n", p->status);
+        printf("Reading %d: ", p - arr);
+        printf("id=%d ", p->id);
+        printf("value=%.1f ", p->value);
+        printf("status=%s\n", status_array[p->status]);
         p++;
     }
+
+   printf("Number of Errors: %d\n", countByStatus(arr, 10, STATUS_ERROR));
+
+   freeReadings(arr);
 }
